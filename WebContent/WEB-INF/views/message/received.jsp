a2f7db25eb458cf6c0501942159e614d11ac9325
AnanyaSubburathinam <ananya.subburathinam@yahoo.com>
2015-04-22 14:54:28 -0400

    Created folder, jsp started

---
+++ WebContent/WEB-INF/views/message/received.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Received</title>
+<link href="/Easybuy/css/common.css" rel="stylesheet" type="text/css" />
+<link href="/Easybuy/css/message.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div class="container">
+		<jsp:include page='../header.jsp'>
+			<jsp:param name="select" value="home" />
+		</jsp:include>
+</div>
+<table border="0" cellpadding="0" cellspacing="0" class="data-table" id="list-table">
+						<tr class="data-table-head">
+							<td width="20%">Type</td>
+							<td width="30%">From</td>
+							<td width="30%">Content</td>
+							<td width="20%">     </td>
+						</tr>
+						<tr>
+							<td>
+</table>
+</body>
+</html>
--- src/com/easybuy/message/MessageController.java
+++ src/com/easybuy/message/MessageController.java
@@ -1,5 +1,43 @@
 package com.easybuy.message;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller("messageController")
+@RequestMapping("/message")
 public class MessageController {
 
+	@RequestMapping(value = "", method = {RequestMethod.GET, RequestMethod.POST})
+	public ModelAndView tolist(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+		ModelAndView mav = new ModelAndView();
+		try {
+			mav.setViewName("/message/received");
+			return mav;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+		}
+		return mav;
+	}
+	
+	@RequestMapping(value = "/received", method = {RequestMethod.GET, RequestMethod.POST})
+	public ModelAndView message(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+		ModelAndView mav = new ModelAndView();
+		try {
+			mav.setViewName("/messaage/received");
+			return mav;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+		}
+		return mav;
+	}
 }
